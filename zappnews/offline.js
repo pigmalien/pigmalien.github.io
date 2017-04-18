afc4670d062026a362620d498073e3029a5e44d7
pigmalien <japigg@bellsouth.net>
2017-04-18 10:49:37 -0400

    zapp
    news

--- zappnews/offline.js
+++ zappnews/offline.js
@@ -1,7 +1,8 @@
 ﻿{
-	"version": 1491429902,
+	"version": 1492526633,
 	"fileList": [
 		"data.js",
+		"game.html",
 		"c2runtime.js",
 		"jquery-2.1.1.min.js",
 		"offlineClient.js",
@@ -22,6 +23,7 @@
 		"images/newsapi-sheet0.png",
 		"images/linkarticle-sheet0.png",
 		"images/tweet-sheet0.png",
+		"images/tiledbackground.png",
 		"icon-16.png",
 		"icon-32.png",
 		"icon-114.png",
