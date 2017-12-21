d74518d356ec0e35022887d8329e55cda4783de2
pigmalien <japigg@bellsouth.net>
2017-12-20 23:54:05 -0500

    update
    list update

--- inmobi/offline.js
+++ inmobi/offline.js
@@ -1,11 +1,10 @@
 ﻿{
-	"version": 1513815658,
+	"version": 1513831852,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
 		"jquery-2.1.1.min.js",
 		"offlineClient.js",
-		"images/sprite-sheet0.png",
 		"icon-16.png",
 		"icon-32.png",
 		"icon-114.png",
