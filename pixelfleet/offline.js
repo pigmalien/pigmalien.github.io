383468f6e440d4a5871119dcc6b140a4cd64fcf9
pigmalien <japigg@bellsouth.net>
2018-10-04 14:35:20 -0400

    demo pixel  spaceships
    Pixel Spaceships demo

---
+++ pixelfleet/offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1538677632,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/frendly-sheet0.png",
+		"images/enemy-sheet0.png",
+		"images/frenbullet-sheet0.png",
+		"images/enembullet-sheet0.png",
+		"images/particles.png",
+		"images/tiledbackground.png",
+		"images/tiledbackground2.png",
+		"images/tiledbackground3.png",
+		"images/tiledbackground4.png",
+		"images/tiledbackground5.png",
+		"images/tiledbackground6.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
