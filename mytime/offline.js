69493613e61d0fbeae387229bee40b818821b114
pigmalien <japigg@bellsouth.net>
2019-12-16 19:08:22 -0500

    clock
    tick tock

---
+++ mytime/offline.js
@@ -0,0 +1,18 @@
+﻿{
+	"version": 1576541208,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/tiledbackground2.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"moment-with-locales.min.js"
+	]
+}
